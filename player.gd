671b0f62bf2864f27bdfa4e51b754a16c1d236b5
Sopheruss <114589476+Sopheruss@users.noreply.github.com>
2024-11-20 11:56:35 +0100

    prototype
    - basic map layer
    - movable character
    
    toDo:
    - objectlayer and collision

---
+++ player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+@export var speed = 200 
+var screen_size 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+		
+	if velocity.length() > 0: 
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else: 
+		$AnimatedSprite2D.stop()
+		
+	position += velocity * delta 
