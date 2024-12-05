999903eaa3deaa78eea74f5c39b96f5332823f99
ipun26ux <jonasschwab2000@gmail.com>
2024-12-05 12:26:38 +0100

    Interaction draft,, not finished
    first steps for interaction

---
+++ interaction_area.gd
@@ -0,0 +1,10 @@
+extends Area2D
+class_name InteractionArea
+
+@export var action_name: String = "interact" # text above object
+
+var interact: Callable = func(): # kann überschrieben werden
+	pass
+	
+ 
+ 
