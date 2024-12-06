2c3b5ec3374a4d8edf007124c3c49c16ad363f0c
djeske2308 <141008404+djeske2308@users.noreply.github.com>
2024-12-06 13:36:54 +0100

    CharacterAnimationBasic
    Characteranimation hinzugefügt. TODO: Charakter soll sich auch schräg bewegen

--- player.gd
+++ player.gd
@@ -12,19 +12,26 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("right"):
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play("walk_right")
 		velocity.x += 1
 	if Input.is_action_pressed("left"):
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("walk_right")
 		velocity.x -= 1
 	if Input.is_action_pressed("down"):
+		$AnimatedSprite2D.play("walk_front")
 		velocity.y += 1
 	if Input.is_action_pressed("up"):
+		$AnimatedSprite2D.play("walk_back")
 		velocity.y -= 1
+	
 		
 	if velocity.length() > 0: 
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
 	else: 
-		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.play("idles_front")
 		
 	position += velocity * delta 
 	
