22458871f186c7453ee38965b5453c9eea9a4ef4
jonas <58268112+b1ttersuess@users.noreply.github.com>
2024-12-08 16:45:42 +0100

    interaction system
    add: interaction (file-structure)
    add: interaction area (script)
    add; interaction manager (script)
    add: auto load the manager script (project settings)
    add: interaction keymapping on "E" (project settings)
    add: interaction with fire animation, new campfire script (script)
    change: add player node to "player" group in order to have references across scenes
    change: collision layer+mask for interaction (1,2)
    
    Areas get placed (instantiated!) where we wanna have interactable objects (e.g. campfire) and have signals that get called when body enters and leaves area.
    The manager handles the complete interaction logic. Comes with an adjustable label (doesnt work correctly rn).
    
    Ref.: https://youtu.be/ajCraxGAeYU

--- interaction/interaction_area/interaction_area.gd
+++ interaction/interaction_area/interaction_area.gd
@@ -6,5 +6,10 @@ class_name InteractionArea
 var interact: Callable = func(): # kann überschrieben werden
 	pass
 	
- 
- 
+
+func _on_body_entered(body: Node2D) -> void:
+	InteractionManager.register_area(self)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	InteractionManager.unregister_area(self)
--- interaction/interaction_manager.gd
+++ interaction/interaction_manager.gd
@@ -16,12 +16,35 @@ func unregister_area(area: InteractionArea):
 	if index != -1:
 		active_areas.remove_at(index) 
 
+
+func _ready():
+	get_tree().node_added.connect(func(_node):
+		player = get_tree().get_first_node_in_group("player")
+	)
+
 func _process(delta):
 	if active_areas.size() > 0 && can_interact:
 		active_areas.sort_custom(_sort_by_distance_to_player)
-		
+		label.text = base_text + active_areas[0].action_name
+		label.global_position = active_areas[0].global_position
+		label.global_position.y -= 36
+		label.global_position.x -= label.size.x / 2
+		label.show()
+	else: 
+		label.hide()
 		
 func _sort_by_distance_to_player(area1,area2):
 	var area1_to_player = player.global_position.distance_to(area1.global_position)
 	var area2_to_player = player.global_position.distance_to(area2.global_position)
 	return  area1_to_player < area2_to_player
+
+
+func _input(event):
+	if event.is_action_pressed("interact") && can_interact:
+		if active_areas.size() > 0:
+			can_interact = false
+			label.hide()
+			
+			await active_areas[0].interact.call() # .call() to call our callabale like a function
+			
+			can_interact = true
