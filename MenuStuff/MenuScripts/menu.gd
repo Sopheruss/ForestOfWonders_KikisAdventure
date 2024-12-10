02d517c2d40bbe719532888d1122b1097e3e3fdd
Sopheruss <114589476+Sopheruss@users.noreply.github.com>
2024-12-10 11:58:06 +0100

    simple menu
    - added simple main menu (renamed UI scene to main menu)
    -> with play, options, quit button (signal to main menu)
    - added options menu scene
    -> only back button right now
    - changed name main to game
    - changed project settings view scale
    - added folders for menu stuff
    -> one for scripts, one for scenes
    
    TODO: Change only viewport of camera in game, not whole game including menus

---
+++ MenuStuff/MenuScripts/menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("esc"):
+		get_tree().quit()
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://game.tscn")
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://optionsMenu.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
--- game.gd
+++ game.gd
@@ -9,4 +9,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("esc"):
-		get_tree().quit()
+		get_tree().change_scene_to_file("res://MenuStuff/MenuScenes/pause_menu.tscn")
