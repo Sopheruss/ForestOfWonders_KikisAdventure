2ae9d159fb7528799e5ad7711149a11505165f4a
ipun26ux <jonasschwab2000@gmail.com>
2024-12-11 17:56:27 +0100

    switched up sample tree interaction
    change: upon interaction, tree and its collision boxes now get disabled/hidden

--- sample_tree.gd
+++ sample_tree.gd
@@ -1,9 +1,14 @@
 extends StaticBody2D
 
-@onready var interaction_area = $InteractionArea # reference
+@onready var interaction_area = $InteractionArea  # Reference to the interaction area
+@onready var sprite = $sprite  # Reference to the tree's Sprite2D
+@onready var collision_shape = $CollisionShape  # Reference to the collision shape
 
 func _ready() -> void:
-	interaction_area.interact = Callable(self, "_sample_Tree")
+	interaction_area.interact = Callable(self, "_sample_Tree")  # Register the interact callback
 
 func _sample_Tree():
-	print ("interaction")
+	print("Tree interacted with")
+	sprite.hide()  # Hide the tree's visual sprite
+	collision_shape.disabled = true  # Disable the collision shape
+	interaction_area.queue_free()  # Optionally remove the interaction area
