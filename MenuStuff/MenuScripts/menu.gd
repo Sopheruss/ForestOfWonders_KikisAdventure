cc56be7c6378bfba24683c5e1e24f2265d9e7555
Sopheruss <114589476+Sopheruss@users.noreply.github.com>
2024-12-11 11:58:01 +0100

    rename game to main

--- MenuStuff/MenuScripts/menu.gd
+++ MenuStuff/MenuScripts/menu.gd
@@ -5,7 +5,7 @@ func _process(delta: float) -> void:
 		get_tree().quit()
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://MenuStuff/MenuScenes/options_menu.tscn")
