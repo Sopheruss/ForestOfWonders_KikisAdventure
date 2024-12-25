c1a824179f1260bdc44e096061354fd8ae85bbd2
Sopheruss <114589476+Sopheruss@users.noreply.github.com>
2024-12-25 11:51:52 +0100

    Credits Scene
    - added Credits Scene
    - linked to Main Menu
    - begun adding crediting stuff
    
    - PLEASE ADD YOUR CREDITS!
    -> each credit has an own lable in the credit scene
    -> set horizontal alignment to center
    -> needs to be child of vBox Container!

--- MenuStuff/MenuScripts/menu.gd
+++ MenuStuff/MenuScripts/menu.gd
@@ -13,6 +13,5 @@ func _on_options_pressed() -> void:
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_credits_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://MenuStuff/MenuScenes/credits.tscn")
