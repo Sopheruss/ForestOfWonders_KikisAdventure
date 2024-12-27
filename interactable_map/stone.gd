7924da156bcf036695798ee531202a888c94ef9b
b1ttersuess <58268112+b1ttersuess@users.noreply.github.com>
2024-12-27 13:39:43 +0100

    add sound effects
    change: adjust player-script in order to support continuous sound / looping
    change: set different parameters for AudioStreamPlayer (pitch scale, volume)
    add: sound effect for crushing stone (AudioStreamPlayer node)
    add: sound effect for chopping tree (AudioStreamPlayer node)
    add: sound effect for walking (AudioStreamPlayer node)

--- interactable_map/stone.gd
+++ interactable_map/stone.gd
@@ -3,11 +3,13 @@ extends StaticBody2D
 @onready var interaction_area = $InteractionArea  # Reference to the interaction area
 @onready var sprite = $sprite  # Reference to the tree's Sprite2D
 @onready var collision_shape = $CollisionShape  # Reference to the collision shape
+@onready var audio_player = $AudioStreamPlayer
 
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "_sample_Stone")  # Register the interact callback
 
 func _sample_Stone():
+	audio_player.play()
 	sprite.hide()  # Hide the tree's visual sprite
 	collision_shape.disabled = true  # Disable the collision shape
 	interaction_area.queue_free()  # Optionally remove the interaction area
--- interactable_map/tree.gd
+++ interactable_map/tree.gd
@@ -2,12 +2,14 @@ extends StaticBody2D
 
 @onready var interaction_area = $InteractionArea  # Reference to the interaction area
 @onready var sprite = $sprite  # Reference to the tree's Sprite2D
-@onready var collision_shape = $CollisionShape  # Reference to the collision shape
+@onready var collision_shape = $CollisionShape  # Reference to the collision 
+@onready var audio_player = $AudioStreamPlayer
 
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "_sample_Tree")  # Register the interact callback
 
 func _sample_Tree():
+	audio_player.play()
 	sprite.hide()  # Hide the tree's visual sprite
 	collision_shape.disabled = true  # Disable the collision shape
 	interaction_area.queue_free()  # Optionally remove the interaction area
