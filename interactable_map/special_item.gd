190932edd9c193fbd009f02c5e9e021a4447cd12
b1ttersuess <58268112+b1ttersuess@users.noreply.github.com>
2024-12-28 16:56:52 +0100

    script changes
    change: descriptive comments

--- interactable_map/special_item.gd
+++ interactable_map/special_item.gd
@@ -3,7 +3,7 @@ extends StaticBody2D
 @onready var interaction_area = $InteractionArea  # Reference to the interaction area
 @onready var sprite = $sprite  # Reference to the tree's Sprite2D
 @onready var collision_shape = $CollisionShape  # Reference to the collision shape
-@onready var audio_player = $AudioStreamPlayer
+@onready var audio_player = $AudioStreamPlayer # Reference to audio stream
 
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "_sample_Special")  # Register the interact callback
--- interactable_map/tree.gd
+++ interactable_map/tree.gd
@@ -3,7 +3,8 @@ extends StaticBody2D
 @onready var interaction_area = $InteractionArea  # Reference to the interaction area
 @onready var sprite = $sprite  # Reference to the tree's Sprite2D
 @onready var collision_shape = $CollisionShape  # Reference to the collision 
-@onready var audio_player = $AudioStreamPlayer
+@onready var audio_player = $AudioStreamPlayer # Reference to audio stream
+
 
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "_sample_Tree")  # Register the interact callback
