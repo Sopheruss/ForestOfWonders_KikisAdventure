77d1f7e22eafa30144f5916bb37f99eacdc0a377
djeske2308 <141008404+djeske2308@users.noreply.github.com>
2025-01-05 01:02:48 +0100

    Sprechblase und Haus Textur hinzugefügt
    - Texturen eingefügt
    - auf Map plaziert
    - Spieler umplaziert

--- housing_logic.gd
+++ housing_logic.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+@export var tent: Texture
+@export var house: Texture
+
+@onready var currentHousing = $HouseOrTent
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	currentHousing.texture = house
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
