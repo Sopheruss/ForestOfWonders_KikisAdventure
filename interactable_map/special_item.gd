807581b0f6833acb0dcaf2eb5fe15fae1d795c1f
Sopheruss <114589476+Sopheruss@users.noreply.github.com>
2025-01-10 10:04:06 +0100

    added sound condition
    - made special item sound more quiet
    - removed background music if special item sound playes

--- interactable_map/special_item.gd
+++ interactable_map/special_item.gd
@@ -11,14 +11,28 @@ var special_item_energy_usage = -5
 
 @onready var player = get_tree().root.get_node("game").get_node("Player")
 
+@onready var backgroundMusic = get_tree().root.get_node("game").get_node("backgroundMusik")
+@onready var playSpecialeffekts = true
+
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "_sample_Special")  # Register the interact callback
+
+func _process(delta: float) -> void:
+	playSpecialeffekts = get_tree().root.get_node("game").get_node("HUD").get_node("PauseMenu").soundEffects
 
 func _sample_Special():
 	sprite.hide()  # Hide the tree's visual sprite
 	player.playMagicAnimation()
 	collision_shape.disabled = true  # Disable the collision shape
+	
+	if not playSpecialeffekts:
+		backgroundMusic.stop()
+	
 	audio_player.play()
 	interaction_area.queue_free()  # Optionally remove the interaction area
 	inventory_bar.add_item(sprite.texture)
 	energy_bar.handleEnergyChange(special_item_energy_usage) # removes 5 energy
+	
+	if not playSpecialeffekts:
+		await get_tree().create_timer(5.0).timeout
+		backgroundMusic.play()
