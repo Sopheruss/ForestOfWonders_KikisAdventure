33b8cb196eff8ed9d2f217ce42059331e991be2b
djeske2308 <141008404+djeske2308@users.noreply.github.com>
2025-01-10 09:52:20 +0100

    Campfire Änderung
    - Y-sort enabled
    - Zaubern beim an und ausmachen

--- campfire.gd
+++ campfire.gd
@@ -2,12 +2,14 @@ extends StaticBody2D
 
 @onready var interaction_area = $InteractionArea # reference
 @onready var animation = $Animation # reference
+@onready var player = get_tree().root.get_node("game").get_node("Player")
 var is_anim_playing = false  # To track the animation state
 
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "_toggle_animation")
 
 func _toggle_animation():
+	player.playMagicAnimation()
 	if is_anim_playing:
 		# Stop the animation
 		animation.stop()
